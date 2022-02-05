228fa41e2fb4b43abeb48737098850b5435fe508
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-02-05 15:20:24 +0530

    added database

--- index.js
+++ index.js
@@ -63,30 +63,26 @@ client.categories = fs.readdirSync("./commands/");
 });
 
 // // database connection
-// const mongoose = require('mongoose')
-// mongoose.connect(`Mongo_URL`)
-// .then(m =>{
-//   console.log(`Database Connected ..`)
-// })
-
-client.on("ready", async () => {
-  require("./loadDB")(client);
+const Enmap = require("enmap");
+client.settings = new Enmap({
+  name: "settings",
 });
+
 client.login(config.token);
 
-// process.on("unhandledRejection", (reason, p) => {
-//   console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
-//   console.log(reason, p);
-// });
-// process.on("uncaughtException", (err, origin) => {
-//   console.log(" [Error_Handling] :: Uncaught Exception/Catch");
-//   console.log(err, origin);
-// });
-// process.on("uncaughtExceptionMonitor", (err, origin) => {
-//   console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
-//   console.log(err, origin);
-// });
-// process.on("multipleResolves", (type, promise, reason) => {
-//   console.log(" [Error_Handling] :: Multiple Resolves");
-//   console.log(type, promise, reason);
-// });
+process.on("unhandledRejection", (reason, p) => {
+  console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
+  console.log(reason, p);
+});
+process.on("uncaughtException", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch");
+  console.log(err, origin);
+});
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
+  console.log(err, origin);
+});
+process.on("multipleResolves", (type, promise, reason) => {
+  console.log(" [Error_Handling] :: Multiple Resolves");
+  console.log(type, promise, reason);
+});
