fc434ca036e8f2d15fbb0f3846f2cef70afcecf5
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-02-16 12:37:04 +0530

    Update command_exapmle.js

--- command_exapmle.js
+++ command_exapmle.js
@@ -1,8 +1,10 @@
 // slash command
 
 const { Command } = require("reconlx");
-const ee = require("../../settings/embed.json");
+const emoji = require("../../settings/emoji.json");
+const embed = require("../../settings/embed.json");
 const config = require("../../settings/config.json");
+
 
 module.exports = new Command({
   // options
@@ -20,6 +22,9 @@ module.exports = new Command({
 
 // message command aka prefix cmd
 const { Message, Client } = require("discord.js");
+const emoji = require("../../settings/emoji.json");
+const embed = require("../../settings/embed.json");
+const config = require("../../settings/config.json");
 
 module.exports = {
   name: "",
