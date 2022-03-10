ffb26fa5ef50791493c0b7e78ae00eec6715aa0c
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-03-10 17:18:42 +0530

    updated slash handler

--- handlers/slash_handler.js
+++ handlers/slash_handler.js
@@ -32,6 +32,8 @@ module.exports = async (client) => {
             });
         }
         if (pull.name) {
+          if (cmd.userPermissions || pull.userPermissions !== [])
+            pull.defaultPermission = false;
           client.commands.set(pull.name, pull);
           client.arrayOfcommands.push(pull);
         } else {
@@ -39,26 +41,61 @@ module.exports = async (client) => {
         }
       }
     });
-
     client.on("ready", async () => {
       try {
-        // await client.guilds.cache
-        //   .get(`903532162236694539`)
-        //   .commands.set(client.arrayOfcommands);
-        await client.application.commands
-          .set(client.arrayOfcommands)
-          .then((s) => {
-            console.log("Successfully reloaded application (/) commands.");
-          })
-          .catch((e) => {
-            console.log(e);
-          });
+        await client.guilds.fetch().catch((e) => { });
+        await client.guilds.cache.forEach(async (guild) => {
+          await guild.commands
+            .set(client.arrayOfcommands)
+            .then(async (cmd) => {
+              function getRoles(commandName) {
+                const permissions = client.arrayOfcommands.find(
+                  (x) => x.name === commandName
+                ).userPermissions;
+                if (!permissions) return null;
+                return guild.roles.cache.filter(
+                  (r) => r.permissions.has(permissions) && !r.managed
+                );
+              }
+              const fullPermissions = cmd.reduce((accumulator, x) => {
+                let roles = getRoles(x.name);
+                if (!roles) return accumulator;
+
+                const permissions = roles.reduce((a, v) => {
+                  return [
+                    ...a,
+                    {
+                      id: v.id,
+                      type: "ROLE",
+                      permission: true,
+                    },
+                  ];
+                }, []);
+
+                return [
+                  ...accumulator,
+                  {
+                    id: x.id,
+                    permissions,
+                  },
+                ];
+              }, []);
+
+              await guild.commands.permissions.set({ fullPermissions });
+            })
+            .catch((e) => {
+              console.log(e);
+            });
+        });
       } catch (e) {
         console.log(e);
       }
     });
 
-    // console.log(` Loaded ${commandcount} commands `);
+    client.on("guildCreate", async (guild) => {
+      await guild.commands.set(client.arrayOfcommands).catch((e) => { });
+    });
+    console.log(`${client.commands.size} Commands Loaded`);
   } catch (e) {
     console.log(e);
   }
@@ -161,7 +198,7 @@ module.exports = async (client) => {
    *
    * @param {Queue} queue
    */
-  client.updateplaymsg = async function (queue) {
+  client.updateplaymsg = async function(queue) {
     let data = client.music.get(queue.textChannel.guildId);
     if (data.enable === false) return;
     let song = queue.songs[0];
@@ -195,7 +232,7 @@ module.exports = async (client) => {
    *
    * @param {Queue} queue
    */
-  client.updatequeuemsg = async function (queue) {
+  client.updatequeuemsg = async function(queue) {
     let data = client.music.get(queue.textChannel.guildId);
     if (data.enable === false) return;
     let channel = await queue.textChannel.guild.channels.cache.get(
@@ -212,7 +249,7 @@ module.exports = async (client) => {
       .map((song, index) => {
         return `\`${index + 1}\` [\`${song.name}\`](${song.url}) __**${
           song.user.tag
-        }**__`;
+          }**__`;
       })
       .join("\n\n")
       .substr(0, 3000);
@@ -234,7 +271,7 @@ module.exports = async (client) => {
    *
    * @param {Guild} guild
    */
-  client.updatemusic = async function (guild) {
+  client.updatemusic = async function(guild) {
     let data = client.music.get(guild.id);
     if (data.enable === false) return;
     let channel = await guild.channels.cache.get(data.channel);
