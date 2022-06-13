4a89e27ce4d12a980f7210c282f6449a99a8349e
Cool Management <100831398+CoolMusic1@users.noreply.github.com>
2022-06-13 21:28:17 +0530

    Update index.js
    Added web page for replit users

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const { TOKEN } = require("./settings/config");
 
 const client = new JUGNU();
 module.exports = client;
+//const express = require('express')
+//const app = express()
+//const port = 3000
+
+//app.get('/', (req, res) => {
+ // res.send('Hello World!')
+//})
+
+//app.listen(port, () => {
+// console.log(`Example app listening on port ${port}`)
+//})
 client.start(TOKEN);
 
 process.on("unhandledRejection", (reason, p) => {
