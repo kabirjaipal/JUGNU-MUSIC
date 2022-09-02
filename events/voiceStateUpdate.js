139444cc8aae5779f13e74cc689865c49fd4be94
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-09-02 20:22:02 +0530

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,3 +1,4 @@
+const { ChannelType, PermissionFlagsBits } = require("discord.js");
 const client = require("../index");
 
 client.on("voiceStateUpdate", async (os, ns) => {
@@ -6,13 +7,15 @@ client.on("voiceStateUpdate", async (os, ns) => {
   // auto speak in stage channel
   if (
     ns.channelId &&
-    ns.channel.type === "GUILD_STAGE_VOICE" &&
+    ns.channel.type === ChannelType.GuildStageVoice &&
     ns.guild.members.me.voice.suppress
   ) {
     if (
-      ns.guild.members.me.permissions.has("SPEAK") ||
+      ns.guild.members.me.permissions.has(PermissionFlagsBits.Speak) ||
       (ns.channel &&
-        ns.channel.permissionsFor(ns.guild.members.me).has("SPEAK"))
+        ns.channel
+          .permissionsFor(ns.guild.members.me)
+          .has(PermissionFlagsBits.Speak))
     ) {
       ns.guild.members.me.voice.setSuppressed(false).catch((e) => {});
     }
