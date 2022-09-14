8e128b1ece9657a1e77e7f46678446de6ea84fd0
Cool Management <100831398+CoolMusic1@users.noreply.github.com>
2022-09-14 20:03:38 +0530

    Update volume.js

--- Commands/Slash/Music/volume.js
+++ Commands/Slash/Music/volume.js
@@ -32,10 +32,10 @@ module.exports = {
   run: async (client, interaction, args, queue) => {
     // Code
     let volume = interaction.options.getNumber("amount");
-    if (volume > 250) {
+    if (volume > 2500) {
       return client.embed(
         interaction,
-        `${client.config.emoji.ERROR} Provide Volume Amount Between 1 - 250  !!`
+        `${client.config.emoji.ERROR} Provide Volume Amount Between 1 - 2500  !!`
       );
     } else {
       await queue.setVolume(volume);
