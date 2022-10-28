3b9c087a35c2bf6b379a70e3c703dd1ad46bfdac
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-10-28 21:04:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,24 @@ require("ffmpeg-static");
 const client = new JUGNU();
 module.exports = client;
 
-require("./Dashboard/server");
-
 client.start(TOKEN);
 
-// process.on("unhandledRejection", (reason, p) => {
-//   console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
-//   console.log(reason, p);
-// });
+process.on("unhandledRejection", (reason, p) => {
+  console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
+  console.log(reason, p);
+});
 
-// process.on("uncaughtException", (err, origin) => {
-//   console.log(" [Error_Handling] :: Uncaught Exception/Catch");
-//   console.log(err, origin);
-// });
+process.on("uncaughtException", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch");
+  console.log(err, origin);
+});
 
-// process.on("uncaughtExceptionMonitor", (err, origin) => {
-//   console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
-//   console.log(err, origin);
-// });
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
+  console.log(err, origin);
+});
 
-// process.on("multipleResolves", (type, promise, reason) => {
-//   console.log(" [Error_Handling] :: Multiple Resolves");
-//   console.log(type, promise, reason);
-// });
-
+process.on("multipleResolves", (type, promise, reason) => {
+  console.log(" [Error_Handling] :: Multiple Resolves");
+  console.log(type, promise, reason);
+});
