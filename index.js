3e3ef6152cdb807eb8c13a7594fb5b9a7c2f5ef2
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-12-15 14:09:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,32 @@ require("ffmpeg-static");
 const client = new JUGNU();
 module.exports = client;
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 client.start(TOKEN);
 
-// process.on("unhandledRejection", (reason, p) => {
-//   console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
-//   console.log(reason, p);
-// });
+process.on("unhandledRejection", (reason, p) => {
+  console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
+  console.log(reason, p);
+});
 
-// process.on("uncaughtException", (err, origin) => {
-//   console.log(" [Error_Handling] :: Uncaught Exception/Catch");
-//   console.log(err, origin);
-// });
+process.on("uncaughtException", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch");
+  console.log(err, origin);
+});
 
-// process.on("uncaughtExceptionMonitor", (err, origin) => {
-//   console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
-//   console.log(err, origin);
-// });
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+  console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
+  console.log(err, origin);
+});
 
