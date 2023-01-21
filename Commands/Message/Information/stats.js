7a248f968876911e18e940c2c26552192f78594a
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2023-01-21 19:44:36 +0530

    Update stats.js

--- Commands/Message/Information/stats.js
+++ Commands/Message/Information/stats.js
@@ -58,7 +58,7 @@ module.exports = {
               },
               {
                 name: `📁 Users`,
-                value: `\`${client.guilds.cache.size} \``,
+                value: `\`${client.users.cache.size} \``,
                 inline: true,
               },
               {
