f65ccf00beae4f76d1d05b44604cf39d66b5ba78
AnirbanBiswasGamer <AnirbanBiswasfb@gmail.com>
2023-05-20 19:07:46 +0600

    messege file updated

--- Commands/Message/Information/ping.js
+++ Commands/Message/Information/ping.js
@@ -1,15 +1,19 @@
-const { Message, PermissionFlagsBits } = require("discord.js");
+const {
+  CommandInteraction,
+  PermissionFlagsBits,
+  ApplicationCommandType,
+} = require("discord.js");
 const BAUL = require("../../../handlers/Client");
 const { Queue } = require("distube");
 
 module.exports = {
   name: "ping",
-  aliases: ["latancy"],
-  description: `get ping of bot`,
+  description: "Get the ping of the bot",
   userPermissions: PermissionFlagsBits.SendMessages,
   botPermissions: PermissionFlagsBits.EmbedLinks,
   category: "Information",
   cooldown: 5,
+  type: ApplicationCommandType.ChatInput,
   inVoiceChannel: false,
   inSameVoiceChannel: false,
   Player: false,
@@ -18,13 +22,24 @@ module.exports = {
   /**
    *
    * @param {BAUL} client
-   * @param {Message} message
+   * @param {CommandInteraction} interaction
    * @param {String[]} args
-   * @param {String} prefix
    * @param {Queue} queue
    */
-  run: async (client, message, args, prefix, queue) => {
-    // Code
-    client.embed(message, `Ping :: \`${client.ws.ping}\``);
+  run: async (client, interaction, args, queue) => {
+    // Calculate the API latency
+    const apiLatency = Math.round(client.ws.ping);
+
+    // Get the current timestamp
+    const timestamp = Date.now();
+
+    // Calculate the overall latency
+    const overallLatency = timestamp - interaction.createdTimestamp;
+
+    // Send the response with the ping information
+    client.embed(
+      interaction,
+      `API Latency: \`${apiLatency}ms\`\nOverall Latency: \`${overallLatency}ms\``
+    );
   },
 };
