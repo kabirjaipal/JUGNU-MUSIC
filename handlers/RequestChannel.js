a3c69c245df28d9f4b6ccd6539a8a7fc1bec8459
kabirsingh2004 <kabirjaipal447@gmail.com>
2024-03-26 13:01:16 +0530

    request channel spamming fixed ✅

--- handlers/RequestChannel.js
+++ handlers/RequestChannel.js
@@ -24,6 +24,7 @@ module.exports = async (client) => {
         }
         // code
         await message.delete().catch((e) => {});
+        if (message.author.bot) return;
 
         let song = message.cleanContent;
 
